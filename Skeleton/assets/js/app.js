f43c30534549cc1414cacb5c9adb0ff7e38fcded
Gryazzz <33532469+Gryazzz@users.noreply.github.com>
2018-06-12 12:06:36 -0400

    in-progress

--- Skeleton/assets/js/app.js
+++ Skeleton/assets/js/app.js
@@ -31,6 +31,96 @@ var svg = d3
 var chartGroup = svg.append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
+
+// add multiaxis
+
+var chosenX = 'alcohol_consumption';
+var chosenY = 'bachelor';
+
+function xScale(alldata, chosenX) {
+    var xLinearScale = d3.scaleLinear()
+        .domain([d3.min(alldata, d => d[chosenX])*0.9, d3.max(alldata, d => d[chosenX])*1.1])
+        .range([0, width]);
+    return xLinearScale;
+}
+
+function yScale(alldata, chosenY) {
+    var yLinearScale = d3.scaleLinear()
+        .domain([d3.min(alldata, d => d[chosenY])*0.9, d3.max(alldata, d => d[chosenY])*1.1]).nice()
+        .range([height, 0]);
+    return yLinearScale;
+}
+
+// function used for updating axis var upon click on axis label
+function renderXAxis(newXScale, xAxis) {
+    var bottomAxis = d3.axisBottom(newXScale);
+  
+    xAxis.transition()
+      .duration(1000)
+      .call(bottomAxis);
+  
+    return xAxis;
+}
+
+function renderYAxis(newYScale, yAxis) {
+    var leftAxis = d3.axisLeft(newYScale);
+  
+    yAxis.transition()
+      .duration(1000)
+      .call(leftAxis);
+  
+    return yAxis;
+}
+
+// function used for updating circles group with a transition to new circles
+function renderCirclesX(circlesGroup, newXScale, chosenX) {
+    circlesGroup.transition()
+      .duration(1000)
+      .attr("cx", d => newXScale(d[chosenX]));
+    return circlesGroup;
+}
+
+function renderCirclesY(circlesGroup, newYScale, chosenY) {
+    circlesGroup.transition()
+      .duration(1000)
+      .attr("cy", d => newYScale(d[chosenY]));
+    return circlesGroup;
+}
+
+// function used for updating circles group with new tooltip
+function updateToolTip(chosenX, circlesGroup) {
+
+    if (chosenX == "alcohol_consumption") {
+      var labelx = "Alcohol Consumption:";
+    }
+    else if (chosenX == "heart_attack"){
+        var labelx = 'Heart Attack:';
+    }
+    else {
+      var labelx = "Depression:";
+    }
+  
+    var toolTip = d3.tip()
+      .attr("class", "tooltip")
+    //   .offset([80, -60])
+      .html(function(d) {
+        return (`${d.state}:<br>${labelx} - ${d.chosenX}<br>${d.chosenY}`);
+        });
+  
+    circlesGroup.call(toolTip);
+  
+    circlesGroup.on("mouseover", function(data) {
+        d3.select(this).style("cursor", "pointer");
+        toolTip.show(data);
+      })
+    .on("mouseout", function(data) {
+        toolTip.hide(data);
+    });
+  
+    return circlesGroup;
+}
+
+
 d3.csv('../data/all_data.csv', (err, alldata) => {
     if (err) throw err;
     
@@ -42,32 +132,45 @@ d3.csv('../data/all_data.csv', (err, alldata) => {
             }
         });
     });
+    // console.log(alldata);
 
-    var xLinearScale = d3.scaleLinear()
-        .domain([d3.min(alldata, d => d.alcohol_consumption)*0.9, d3.max(alldata, d => d.alcohol_consumption)*1.1])
-        .range([0, width]);
+    var xLinearScale = xScale(alldata, chosenX);
+    var yLinearScale = yScale(alldata, chosenY);
+    
 
-    var yLinearScale = d3.scaleLinear()
-        .domain([d3.min(alldata, d => d.bachelor)*0.9, d3.max(alldata, d => d.bachelor)*1.1]).nice()
-        .range([height, 0]);
+    // var yLinearScale = d3.scaleLinear()
+    //     .domain([d3.min(alldata, d => d.bachelor)*0.9, d3.max(alldata, d => d.bachelor)*1.1]).nice()
+    //     .range([height, 0]);
 
-    var xAxis = d3.axisBottom(xLinearScale);
-    var yAxis = d3.axisLeft(yLinearScale);
-
-    chartGroup.append("g")
+    var bottomAxis = d3.axisBottom(xLinearScale);
+    var leftAxis = d3.axisLeft(yLinearScale);
+    
+    var xAxis = chartGroup.append("g")
+        .classed("x-axis", true)
         .attr("transform", `translate(10, ${height-5})`)
-        .call(xAxis);
-
-    chartGroup.append("g")
+        .call(bottomAxis);
+    
+    var yAxis = chartGroup.append("g")
         .attr("transform", `translate(10, -5)`)
-        .call(yAxis);
+        .call(leftAxis);
+
+    
+
+
+    // chartGroup.append("g")
+    //     .attr("transform", `translate(10, ${height-5})`)
+    //     .call(xAxis);
+
+    // chartGroup.append("g")
+    //     .attr("transform", `translate(10, -5)`)
+    //     .call(yAxis);
 
     var circlesGroup = chartGroup.selectAll("circle")
         .data(alldata)
         .enter()
         .append("circle")
-        .attr("cx", d => xLinearScale(d.alcohol_consumption))
-        .attr("cy", d => yLinearScale(d.bachelor))
+        .attr("cx", d => xLinearScale(d[chosenX]))
+        .attr("cy", d => yLinearScale(d[chosenY]))
         .attr("r", "10")
         .style("fill", "violet")
         .attr("opacity", ".7")
@@ -79,57 +182,155 @@ d3.csv('../data/all_data.csv', (err, alldata) => {
         .append('text')
 
     var textLabels = circleText
-        .attr('x', d => xLinearScale(d.alcohol_consumption))
-        .attr('y', d => yLinearScale(d.bachelor)+4)
+        .attr('x', d => xLinearScale(d[chosenX]))
+        .attr('y', d => yLinearScale(d[chosenY])+4)
         .attr("text-anchor", "middle")
         .text(d => d.state_abbr)
         .attr("font-size", "10px")
         .attr('font-weight', 'bold')
         .attr("fill", "blue");
-        
+    
+    var xlabelsGroup = chartGroup.append("g")
+        .attr("transform", `translate(${width / 2}, ${height + 10})`);
 
-    var toolTip = d3.tip()
-        .attr("class", "tooltip")
-        // .offset([20, -30])
-        .html(function(d) {
-            return (`${d.state}:<br>Alcohol consumption - ${d.alcohol_consumption}<br>Bachelor - ${d.bachelor}`);
-        });
+    var xLabel1 = xlabelsGroup.append("text")
+        .attr("x", 0)
+        .attr("y", 20)
+        .attr("value", "alcohol_consumption") // value to grab for event listener
+        .classed("active", true)
+        .text("Alcohol Consumption (%)");
 
-    chartGroup.call(toolTip);
+    var xLabel2 = xlabelsGroup.append("text")
+        .attr("x", 0)
+        .attr("y", 40)
+        .attr("value", "heart_attack")
+        .classed("inactive", true)
+        .text("Heart Attack (%)");
 
-    circlesGroup.on("mouseover", function(data) {
-        d3.select(this).style("cursor", "pointer");
-        toolTip.show(data);
-      })
-    .on("mouseout", function(data) {
-        toolTip.hide(data);
-    });
+    var xLabel3 = xlabelsGroup.append("text")
+        .attr("x", 0)
+        .attr("y", 60)
+        .attr("value", "depression")
+        .classed("inactive", true)
+        .text("Depression (%)");
+    
+    var ylabelsGroup = chartGroup.append("g")
+        .attr("transform", `translate(${height / 2}, 0-${margin.left})`);
 
-    textLabels.on("mouseover", function(data) {
-        d3.select(this). style("cursor", "pointer");
-        toolTip.show(data);
-      })
-      .on("mouseout", function(data) {
-        
-        toolTip.hide(data);
-    });
-
-    chartGroup.append("text")
-        .attr("text-anchor", "middle")
+    var yLabel1 = ylabelsGroup.append('text')
         .attr("transform", "rotate(-90)")
-        
-        .attr("y", 0 - margin.left*0.33)
+        .attr("y", 0 - margin.left+60)
         .attr("x", 0 - (height / 2))
         .attr("dy", "1em")
-        .attr("class", "axisText")
-        .text("Alcohol consumption (%)");
-
-    chartGroup.append("text")
-        .attr("text-anchor", "middle")
-        .attr("transform", "translate("+ (width/2) + "," + (height + margin.bottom*0.33) + ")")
-        .attr("class", "axisText")
-        .text("Bachelor degree holders (%)");
-
-
+        .attr("class", "active")
+        .text("Bachelor Degree Holders (%)");
     
-});
+    var yLabel2 = ylabelsGroup.append('text')
+        .attr("transform", "rotate(-90)")
+        .attr("y", 0 - margin.left+40)
+        .attr("x", 0 - (height / 2))
+        .attr("dy", "1em")
+        .attr("class", "inactive")
+        .text("Second Label (%)");
+    
+    var yLabel3 = ylabelsGroup.append('text')
+        .attr("transform", "rotate(-90)")
+        .attr("y", 0 - margin.left+20)
+        .attr("x", 0 - (height / 2))
+        .attr("dy", "1em")
+        .attr("class", "inactive")
+        .text("Third Label (%)");
+    
+    var circlesGroup = updateToolTip(chosenX, circlesGroup);
+
+    xlabelsGroup.selectAll("text")
+    .on("click", function() {
+      // get value of selection
+      var value = d3.select(this).attr("value");
+      if (value != chosenX) {
+
+        // replaces chosenXAxis with value
+        chosenX = value;
+        xLinearScale = xScale(alldata, chosenX);
+        xAxis = renderXAxis(xLinearScale, xAxis);
+        circlesGroup = renderCirclesX(circlesGroup, xLinearScale, chosenX);
+        circlesGroup = updateToolTip(chosenX, circlesGroup);
+
+        if (chosenX == "alcohol_consumption") {
+            xLabel1
+              .classed("active", true)
+              .classed("inactive", false);
+            xLabel2
+              .classed("active", false)
+              .classed("inactive", true);
+            xLabel3
+              .classed("active", false)
+              .classed("inactive", true);
+        }
+        else if (chosenX == "heart_attack") {
+            xLabel2
+              .classed("active", true)
+              .classed("inactive", false);
+            xLabel1
+              .classed("active", false)
+              .classed("inactive", true);
+            xLabel3
+              .classed("active", false)
+              .classed("inactive", true);
+        }
+        else {
+            xLabel3
+              .classed("active", true)
+              .classed("inactive", false);
+            xLabel2
+              .classed("active", false)
+              .classed("inactive", true);
+            xLabel1
+              .classed("active", false)
+              .classed("inactive", true);
+        }
+    }
+    });    
+})
+
+    // var toolTip = d3.tip()
+    //     .attr("class", "tooltip")
+    //     // .offset([20, -30])
+    //     .html(function(d) {
+    //         return (`${d.state}:<br>Alcohol consumption - ${d.alcohol_consumption}<br>Bachelor - ${d.bachelor}`);
+    //     });
+
+    // chartGroup.call(toolTip);
+
+    // circlesGroup.on("mouseover", function(data) {
+    //     d3.select(this).style("cursor", "pointer");
+    //     toolTip.show(data);
+    //   })
+    // .on("mouseout", function(data) {
+    //     toolTip.hide(data);
+    // });
+
+    // textLabels.on("mouseover", function(data) {
+    //     d3.select(this). style("cursor", "pointer");
+    //     toolTip.show(data);
+    //   })
+    //   .on("mouseout", function(data) {
+        
+    //     toolTip.hide(data);
+    // });
+
+    // chartGroup.append("text")
+    //     .attr("text-anchor", "middle")
+    //     .attr("transform", "rotate(-90)")
+        
+    //     .attr("y", 0 - margin.left*0.33)
+    //     .attr("x", 0 - (height / 2))
+    //     .attr("dy", "1em")
+    //     .attr("class", "axisText")
+    //     .text("Alcohol consumption (%)");
+
+    // chartGroup.append("text")
+    //     .attr("text-anchor", "middle")
+    //     .attr("transform", "translate("+ (width/2) + "," + (height + margin.bottom*0.33) + ")")
+    //     .attr("class", "axisText")
+    //     .text("Bachelor degree holders (%)");
