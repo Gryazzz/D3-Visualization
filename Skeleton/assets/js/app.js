997152390e5bdec5dbeb7894be3b65f17a3dd8f1
Gryazzz <33532469+Gryazzz@users.noreply.github.com>
2018-06-11 21:00:18 -0400

    in-progress

--- Skeleton/assets/js/app.js
+++ Skeleton/assets/js/app.js
@@ -15,8 +15,8 @@ var svgHeight = 500;
 var margin = {
   top: 20,
   right: 20,
-  bottom: 20,
-  left: 20
+  bottom: 50,
+  left: 50
 };
 
 var width = svgWidth - margin.left - margin.right;
@@ -42,6 +42,68 @@ d3.csv('../data/all_data.csv', (err, alldata) => {
             }
         });
     });
-    
+
+    var xLinearScale = d3.scaleLinear()
+        .domain([d3.min(alldata, d => d.alcohol_consumption)-10, d3.max(alldata, d => d.alcohol_consumption)+10])
+        .range([0, width]);
+
+    var yLinearScale = d3.scaleLinear()
+        .domain([d3.min(alldata, d => d.bachelor)-10, d3.max(alldata, d => d.bachelor)+10]).nice()
+        .range([height, 0]);
+
+    var xAxis = d3.axisBottom(xLinearScale);
+    var yAxis = d3.axisLeft(yLinearScale);
+
+    chartGroup.append("g")
+        .attr("transform", `translate(0, ${height})`)
+        .call(xAxis);
+
+    chartGroup.append("g")
+    .   call(yAxis);
+
+    var circlesGroup = chartGroup.selectAll("circle")
+        .data(alldata)
+        .enter()
+        .append("circle")
+        .attr("cx", d => xLinearScale(d.alcohol_consumption))
+        .attr("cy", d => yLinearScale(d.bachelor))
+        .attr("r", "13")
+        .attr("fill", "violet")
+        .attr("opacity", ".7")
+        .text((d => d.data));
+
+    var toolTip = d3.tip()
+        .attr("class", "tooltip")
+        .offset([20, -30])
+        .html(function(d) {
+            return (`${d.state}<br> Alcohol consumption: ${d.alcohol_consumption} <br> Bachelor: ${d.bachelor}`);
+        });
+
+    chartGroup.call(toolTip);
+
+    circlesGroup.on("click", function(data) {
+            toolTip.show(data);
+        })
+        .on("mouseout", function(data, index) {
+            toolTip.hide(data);
+        });
+
+    chartGroup.append("text")
+        .attr("text-anchor", "middle")
+        .attr("transform", "rotate(-90)")
+        
+        .attr("y", 0 - margin.left + 10)
+        .attr("x", 0 - (height / 2))
+        .attr("dy", "1em")
+        .attr("class", "axisText")
+        .text("Alcohol consumption (%)");
+
+    chartGroup.append("text")
+        .attr("text-anchor", "middle")
+        .attr("transform", "translate("+ (width/2) + "," + (height + 35) + ")")
+        .attr("class", "axisText")
+        .text("Bachelor degree holders (%)");
+
+        
     
 });
